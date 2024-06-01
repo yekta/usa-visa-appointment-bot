4f4f29528f9b3be371955036865d880f3ab09405
yekta <ngyekta@gmail.com>
2024-06-01 03:08:25 +0300

    Try 5 second interval

--- src/earliestDate.ts
+++ src/earliestDate.ts
@@ -16,7 +16,7 @@ import { consoleLog } from "@/utils";
 import moment from "moment-timezone";
 import { Page } from "puppeteer";
 
-const earliestDatePullDelays = [10000, 11000];
+const earliestDatePullDelays = [5000, 6000];
 
 export async function continuouslyGetEarliestDate({
   page,
